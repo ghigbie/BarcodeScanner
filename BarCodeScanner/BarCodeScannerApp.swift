7bd04b5d1cfcc8df81e349ec0acac898d7279015
ghigbie <gehigbie@gmail.com>
2021-01-31 20:27:27 -0500

    Initial commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by George Higbie on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
